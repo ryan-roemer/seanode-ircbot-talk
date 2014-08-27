20f0a2da578abdbe903dfad8b4cfa54eee022192
Ryan Roemer <ryan.roemer@formidablelabs.com>
2014-08-26 19:37:33 -0700

    Rainbow

--- bot.js
+++ bot.js
@@ -1,13 +1,25 @@
 var irc = require("irc");
+var _ = require("lodash");
 
-var bot = new irc.Client("irc.freenode.net", "SemicolonBot", {
-  channels: ["#seanode"]
+var RAINBOW = [
+  "white",
+  "light_red",
+  "orange",
+  "yellow",
+  "light_green",
+  "light_blue",
+  "light_magenta"
+];
+
+var bot = new irc.Client("irc.freenode.net", "RainbowBot", {
+  channels: ["#seanode"],
+  floodProtection: true
 });
 
 // Log to console.
-bot.on("message", function (from, to, msg) {
-  console.log("%s said %s to %s", from, msg, to);
-});
+// bot.on("message", function (from, to, msg) {
+//   console.log("%s said %s to %s", from, msg, to);
+// });
 
 // bot.on("message", function (from, to, msg) {
 //   if (/[^\;]$/.test(msg)) {
@@ -15,4 +27,18 @@ bot.on("message", function (from, to, msg) {
 //   }
 // });
 
+bot.on("message", function (from, to, msg) {
+  //var result = /^!rainbow (.*)$/.exec(message)
+  var result = /^!shout (.*)$/.exec(msg)
+  if (result) {
+    var rain = _.map(result[1], function (chr, i) {
+      if (chr === " ") { return " "; };
 
+      var color = RAINBOW[i % RAINBOW.length];
+      return irc.colors.codes[color] + chr
+    }).join("");
+
+    console.log(to, rain);
+    bot.say(to, rain);
+  }
+});
