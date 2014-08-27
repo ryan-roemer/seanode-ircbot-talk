4fa72979ecff11fa6ff981792ad4e42d531ab526
Ryan Roemer <ryan.roemer@formidablelabs.com>
2014-08-26 19:11:56 -0700

    Add nodemon and semicolon bot

--- bot.js
+++ bot.js
@@ -1,9 +1,18 @@
 var irc = require("irc");
 
-var bot = new irc.Client("irc.freenode.net", "RyanBot", {
+var bot = new irc.Client("irc.freenode.net", "SemicolonBot", {
   channels: ["#seanode"]
 });
 
+// Log to console.
 bot.on("message", function (from, to, msg) {
   console.log("%s said %s to %s", from, msg, to);
 });
+
+// bot.on("message", function (from, to, msg) {
+//   if (/[^\;]$/.test(msg)) {
+//     bot.say(to, msg + "; // There. Added a semicolon for you ;)");
+//   }
+// });
+
+
